eab11ef5d006057c126ea1967eee7b6ff4f69025
boberro <m.cyraniak@gmail.com>
2017-12-25 02:25:34 +0100

    config reformat (name, author etc.)

--- config.cpp
+++ config.cpp
@@ -1,439 +1,441 @@
 class CfgPatches
 {
-  class Markers
+  class FolkMarkers
   {
     units[] = {};
     weapons[] = {};
     requiredVersion=1.00;
+    author = "Republic of Folk";
+    url = "http://republicoffolk.org";
   };
 };
 
- class CfgMarkerClasses {
-     class FolkMarkers {
-         displayName="Folk Markers";
-     };
- };
+class CfgMarkerClasses {
+   class FolkMarkers {
+       displayName="Folk Markers";
+   };
+};
 
- class CfgMarkers
- {
-    class folk_flag_rof_simple
-    {
-      name="Republic of Folk";
-      icon="\folk_markers\icons\flag_rof_simple.paa";
-      color[]={1,1,1,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_152mm_gun_battery
-    {
-      name="152mm Gun Battery";
-      icon="\folk_markers\icons\152mm_gun_battery.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_ags_17
-    {
-      name="AGS-17";
-      icon="\folk_markers\icons\ags-17.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_airstrike
-    {
-      name="Airstrike";
-      icon="\folk_markers\icons\airstrike.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_ambush
-    {
-      name="Ambush";
-      icon="\folk_markers\icons\ambush.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_attack_company
-    {
-      name="Attack (Company)";
-      icon="\folk_markers\icons\attack_company.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-     class folk_attack_platoon
-    {
-      name="Attack (Platoon)";
-      icon="\folk_markers\icons\attack_platoon.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_biohazard
-    {
-      name="Biohazard";
-      icon="\folk_markers\icons\biohazard.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_bmd_or_btr
-    {
-      name="BMD or BTR";
-      icon="\folk_markers\icons\bmd_or_btr.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_bmp
-    {
-      name="BMP";
-      icon="\folk_markers\icons\bmp.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_bronegruppa
-    {
-      name="Bronegruppa";
-      icon="\folk_markers\icons\bronegruppa.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_claymore
-    {
-      name="Claymore";
-      icon="\folk_markers\icons\claymore.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_claymore_remote
-    {
-      name="Claymore (Remote)";
-      icon="\folk_markers\icons\claymore_remote.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_command_post_company
-    {
-      name="Command Post (Company)";
-      icon="\folk_markers\icons\command_post_company.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_command_post_platoon
-    {
-      name="Command Post (Platoon)";
-      icon="\folk_markers\icons\command_post_platoon.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_defensive_position
-    {
-      name="Defensive Position";
-      icon="\folk_markers\icons\defensive_position.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_defensive_position_enemy
-    {
-      name="Defensive Position (Enemy)";
-      icon="\folk_markers\icons\defensive_position_enemy.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_emission
-    {
-      name="Emission";
-      icon="\folk_markers\icons\emission.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_gunship_strike
-    {
-      name="Gunship Strike";
-      icon="\folk_markers\icons\gunship_strike.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_hazard
-    {
-      name="Hazard";
-      icon="\folk_markers\icons\hazard.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_heavy_aa_mg
-    {
-      name="Heavy AA MG";
-      icon="\folk_markers\icons\heavy_aa_mg.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_heavy_mg
-    {
-      name="Heavy MG";
-      icon="\folk_markers\icons\heavy_mg.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_helibourne_landing
-    {
-      name="Helibourne Landing";
-      icon="\folk_markers\icons\helibourne_landing.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_helicopter
-    {
-      name="Helicopter";
-      icon="\folk_markers\icons\helicopter.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_helicopter_alt
-    {
-      name="Helicopter (Alternative)";
-      icon="\folk_markers\icons\helicopter_alt.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_helicopter_landing_site
-    {
-      name="Helicopter Landing Site";
-      icon="\folk_markers\icons\helicopter_landing_site.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_high_voltage
-    {
-      name="High Voltage";
-      icon="\folk_markers\icons\high_voltage.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_law
-    {
-      name="LAW";
-      icon="\folk_markers\icons\law.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_light_aa_mg
-    {
-      name="Light AA MG";
-      icon="\folk_markers\icons\light_aa_mg.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_light_mg
-    {
-      name="Light MG";
-      icon="\folk_markers\icons\light_mg.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_medium_mg
-    {
-      name="Medium MG";
-      icon="\folk_markers\icons\medium_mg.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_mortar
-    {
-      name="Mortar";
-      icon="\folk_markers\icons\mortar.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_mrls_battery
-    {
-      name="MRLS Battery";
-      icon="\folk_markers\icons\mrls_battery.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_nonarmoured_vehicle
-    {
-      name="Nonarmoured Vehicle";
-      icon="\folk_markers\icons\nonarmoured_vehicle.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_observation_post
-    {
-      name="Observation Post";
-      icon="\folk_markers\icons\observation_post.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_patrol
-    {
-      name="Patrol";
-      icon="\folk_markers\icons\patrol.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_radiation
-    {
-      name="Radiation";
-      icon="\folk_markers\icons\radiation.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_rpg_7
-    {
-      name="RPG-7";
-      icon="\folk_markers\icons\rpg-7.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_skull
-    {
-      name="Skull";
-      icon="\folk_markers\icons\skull.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_small_arms
-    {
-      name="Small Arms";
-      icon="\folk_markers\icons\small_arms.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_tank
-    {
-      name="Tank";
-      icon="\folk_markers\icons\tank.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
-    class folk_truck
-    {
-      name="Truck";
-      icon="\folk_markers\icons\truck.paa";
-      color[]={1,0,0,1};
-      size=32;
-      shadow = 0;
-      scope = 2;
-      markerClass = "FolkMarkers";
-    };
- };
+class CfgMarkers
+{
+  class folk_flag_rof_simple
+  {
+    name="Republic of Folk";
+    icon="\folk_markers\icons\flag_rof_simple.paa";
+    color[]={1,1,1,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_152mm_gun_battery
+  {
+    name="152mm Gun Battery";
+    icon="\folk_markers\icons\152mm_gun_battery.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_ags_17
+  {
+    name="AGS-17";
+    icon="\folk_markers\icons\ags-17.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_airstrike
+  {
+    name="Airstrike";
+    icon="\folk_markers\icons\airstrike.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_ambush
+  {
+    name="Ambush";
+    icon="\folk_markers\icons\ambush.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_attack_company
+  {
+    name="Attack (Company)";
+    icon="\folk_markers\icons\attack_company.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+   class folk_attack_platoon
+  {
+    name="Attack (Platoon)";
+    icon="\folk_markers\icons\attack_platoon.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_biohazard
+  {
+    name="Biohazard";
+    icon="\folk_markers\icons\biohazard.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_bmd_or_btr
+  {
+    name="BMD or BTR";
+    icon="\folk_markers\icons\bmd_or_btr.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_bmp
+  {
+    name="BMP";
+    icon="\folk_markers\icons\bmp.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_bronegruppa
+  {
+    name="Bronegruppa";
+    icon="\folk_markers\icons\bronegruppa.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_claymore
+  {
+    name="Claymore";
+    icon="\folk_markers\icons\claymore.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_claymore_remote
+  {
+    name="Claymore (Remote)";
+    icon="\folk_markers\icons\claymore_remote.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_command_post_company
+  {
+    name="Command Post (Company)";
+    icon="\folk_markers\icons\command_post_company.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_command_post_platoon
+  {
+    name="Command Post (Platoon)";
+    icon="\folk_markers\icons\command_post_platoon.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_defensive_position
+  {
+    name="Defensive Position";
+    icon="\folk_markers\icons\defensive_position.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_defensive_position_enemy
+  {
+    name="Defensive Position (Enemy)";
+    icon="\folk_markers\icons\defensive_position_enemy.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_emission
+  {
+    name="Emission";
+    icon="\folk_markers\icons\emission.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_gunship_strike
+  {
+    name="Gunship Strike";
+    icon="\folk_markers\icons\gunship_strike.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_hazard
+  {
+    name="Hazard";
+    icon="\folk_markers\icons\hazard.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_heavy_aa_mg
+  {
+    name="Heavy AA MG";
+    icon="\folk_markers\icons\heavy_aa_mg.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_heavy_mg
+  {
+    name="Heavy MG";
+    icon="\folk_markers\icons\heavy_mg.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_helibourne_landing
+  {
+    name="Helibourne Landing";
+    icon="\folk_markers\icons\helibourne_landing.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_helicopter
+  {
+    name="Helicopter";
+    icon="\folk_markers\icons\helicopter.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_helicopter_alt
+  {
+    name="Helicopter (Alternative)";
+    icon="\folk_markers\icons\helicopter_alt.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_helicopter_landing_site
+  {
+    name="Helicopter Landing Site";
+    icon="\folk_markers\icons\helicopter_landing_site.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_high_voltage
+  {
+    name="High Voltage";
+    icon="\folk_markers\icons\high_voltage.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_law
+  {
+    name="LAW";
+    icon="\folk_markers\icons\law.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_light_aa_mg
+  {
+    name="Light AA MG";
+    icon="\folk_markers\icons\light_aa_mg.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_light_mg
+  {
+    name="Light MG";
+    icon="\folk_markers\icons\light_mg.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_medium_mg
+  {
+    name="Medium MG";
+    icon="\folk_markers\icons\medium_mg.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_mortar
+  {
+    name="Mortar";
+    icon="\folk_markers\icons\mortar.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_mrls_battery
+  {
+    name="MRLS Battery";
+    icon="\folk_markers\icons\mrls_battery.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_nonarmoured_vehicle
+  {
+    name="Nonarmoured Vehicle";
+    icon="\folk_markers\icons\nonarmoured_vehicle.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_observation_post
+  {
+    name="Observation Post";
+    icon="\folk_markers\icons\observation_post.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_patrol
+  {
+    name="Patrol";
+    icon="\folk_markers\icons\patrol.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_radiation
+  {
+    name="Radiation";
+    icon="\folk_markers\icons\radiation.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_rpg_7
+  {
+    name="RPG-7";
+    icon="\folk_markers\icons\rpg-7.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_skull
+  {
+    name="Skull";
+    icon="\folk_markers\icons\skull.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_small_arms
+  {
+    name="Small Arms";
+    icon="\folk_markers\icons\small_arms.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_tank
+  {
+    name="Tank";
+    icon="\folk_markers\icons\tank.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+  class folk_truck
+  {
+    name="Truck";
+    icon="\folk_markers\icons\truck.paa";
+    color[]={1,0,0,1};
+    size=32;
+    shadow = 0;
+    scope = 2;
+    markerClass = "FolkMarkers";
+  };
+};
